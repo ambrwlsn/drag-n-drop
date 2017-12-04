7a07d6a710be2974a4a05ce646cc4c902ff7eceb
ambrwlsn <arw89@uclive.ac.nz>
2017-12-04 16:04:47 +0000

    deleted old code

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// function allowDrop(ev) {
-//  ev.preventDefault();
-// }
-//
-// function drag(ev) {
-//  console.log("dragStart");
-//  ev.dataTransfer.setData("content", ev.target.id);
-// }
-//
-// function drop(ev) {
-//  ev.preventDefault();
-//  var data = ev.dataTransfer.getData("content");
-//  ev.target.appendChild(document.getElementById(data));
-// }
-
 var dragSrcEl = null;
 
 function handleDragStart(e) {
